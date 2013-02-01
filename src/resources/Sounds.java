1c043da5d19b6c9f2b5c218a929af1ec78e39d95
SignalSin <michaelhenderson.it@gmail.com>
2013-02-01 17:40:01 +0800

    Add Sounds class

---
+++ src/resources/Sounds.java
@@ -0,0 +1,30 @@
+package resources;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Sound;
+
+public class Sounds {
+	
+	public static Sound key;
+	
+	public static void load(){
+		
+		//load the sounds into the variables
+		key = loadSound("key.ogg");
+		
+	}
+	
+    private static Sound loadSound (String filename) { 
+    	//load the sound into the game
+        return Gdx.audio.newSound(Gdx.files.internal("data/" + filename));  
+    }
+      
+    public static void play (Sound sound) {  
+        sound.play(1);  
+    }
+    
+    public static void dispose(){
+    	key.dispose();
+    }
+
+}
